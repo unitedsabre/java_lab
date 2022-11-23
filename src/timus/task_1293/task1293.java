e3071805baa4558a4e3fc4ae6a8eb3e82aee6e14
unitedsabre <biryuchev98@mail.ru>
2022-11-23 19:43:30 +0500

    Initial commit

---
+++ src/timus/task_1293/task1293.java
@@ -0,0 +1,24 @@
+package timus.task_1293;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task1293 {
+
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+            PrintWriter out = new PrintWriter(System.out);
+
+            int N = in.nextInt();
+            int A = in.nextInt();
+            int B = in.nextInt();
+
+            int result = (N * (A*B)) * 2;
+
+            out.println(result);
+            out.flush();
+
+        }
+    }
+
