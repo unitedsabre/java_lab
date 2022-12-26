d6914e203c20e001da1cf408ef9bcd7459d58c53
unitedsabre <biryuchev98@mail.ru>
2022-12-26 18:35:50 +0500

    timus

--- src/timus/task_1293/task1293.java
+++ src/timus/task_1293/task1293.java
@@ -1,24 +1,15 @@
 package timus.task_1293;
-
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class task1293 {
-
         public static void main(String[] args) {
-
             Scanner in = new Scanner(System.in);
-            PrintWriter out = new PrintWriter(System.out);
-
             int N = in.nextInt();
             int A = in.nextInt();
             int B = in.nextInt();
 
             int result = (N * (A*B)) * 2;
 
-            out.println(result);
-            out.flush();
-
+            System.out.println(result);
         }
     }
-
